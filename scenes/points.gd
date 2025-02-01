00751dc8f4f19984089f2dc466c83aeda0940a86
Michael J. Kozubal Jr <mjkjr84@gmail.com>
2025-02-01 09:06:22 -0500

    Tweaked points indicator

--- scenes/points.gd
+++ scenes/points.gd
@@ -9,5 +9,9 @@ func set_score(score: int) -> void:
 	$Label.text = str(score)
 
 
+func _set_pivot() -> void:
+	$Label.pivot_offset = $Label.size * 0.5
+
+
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	queue_free()
